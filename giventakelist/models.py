54ca8aa19b089c7c0c4068e270740b26ccde6385
Maroof Ahmad <ahmadmaroof8@gmail.com>
2016-08-04 22:55:37 +0530

    models working fine with dates

--- giventakelist/models.py
+++ giventakelist/models.py
@@ -1,16 +1,16 @@
 from __future__ import unicode_literals
 from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
 
 # Create your models here.
 @python_2_unicode_compatible
 class ListPost(models.Model):
     item = models.CharField(max_length=200, default="unnamed object")
-    pub_date = models.DateTimeField('date',default=datetime.now())
+    pub_date = models.DateTimeField('date',default=timezone.now)
     person = models.CharField(max_length=200, default= "anonymous person")
     ifreturned = models.BooleanField(default=False)
-    return_date = models.DateTimeField('return date',blank=True)
+    return_date = models.DateTimeField('return date',blank=True,null=True)
     lendOrPost = models.BooleanField(default=False)
     def __str__(self):
         return (self.item)
