676d8aa12318f76b0368cd944cf0580939ecc60a
Maroof Ahmad <ahmadmaroof8@gmail.com>
2016-08-04 22:17:42 +0530

    lend model changed

--- lendlist/models.py
+++ lendlist/models.py
@@ -6,9 +6,10 @@ from django.db import models
 @python_2_unicode_compatible
 class LendPost(models.Model):
     item = models.CharField(max_length=200, default="unnamed object")
-    pub_date = models.DateTimeField('lending date')
+    pub_date = models.DateTimeField('date')
     person = models.CharField(max_length=200, default= "anonymous person")
-    ifreturned = models.BooleanField(default=0)
+    ifreturned = models.BooleanField(default=False)
     return_date = models.DateTimeField('return date')
+    lendOrPost = models.BooleanField(default=False)
     def __str__(self):
         return (self.item)
